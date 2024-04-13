494177a29dc277eccc11d20ea94499e2deb015a6
ZAKHARY Mina <85484181+MinaZA@users.noreply.github.com>
2024-04-13 14:07:20 +0200

    exo9_spring

---
+++ learning_java/src/main/java/fr/hetic/Exo9/Bbd_file.java
@@ -0,0 +1,60 @@
+package fr.hetic.Exo9;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.*;
+
+public class Bbd_file {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Bbd_file reader = (Bbd_file) context.getBean("reader");
+        reader.readFiles();
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+
+    private void readFiles() {
+        String url = "jdbc:postgresql://SG-hetic-mt4-java-5275-pgsql-master.servers.mongodirector.com:5432/TP";
+        String user = "etudiant";
+        String password = "MT4@hetic2324";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            String selectFilesQuery = "SELECT ID, NOM FROM FICHIER WHERE TYPE = 'OP'";
+            try (PreparedStatement filesStatement = connection.prepareStatement(selectFilesQuery)) {
+                ResultSet filesResult = filesStatement.executeQuery();
+                while (filesResult.next()) {
+                    int fileId = filesResult.getInt("ID");
+                    String fileName = filesResult.getString("NOM");
+                    String fileContent = generateFileContent(connection, fileId);
+                    writeFile(fileName, fileContent);
+                }
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateFileContent(Connection connection, int fileId) throws SQLException {
+        StringBuilder contentBuilder = new StringBuilder();
+        String selectLinesQuery = "SELECT PARAM1, PARAM2, OPERATEUR FROM LIGNE WHERE FICHIER_ID = ?";
+        try (PreparedStatement linesStatement = connection.prepareStatement(selectLinesQuery)) {
+            linesStatement.setInt(1, fileId);
+            ResultSet linesResult = linesStatement.executeQuery();
+            while (linesResult.next()) {
+                int param1 = linesResult.getInt("PARAM1");
+                int param2 = linesResult.getInt("PARAM2");
+                char operator = linesResult.getString("OPERATEUR").charAt(0);
+                contentBuilder.append(param1).append(" ").append(param2).append(" ").append(operator).append("\n");
+            }
+        }
+        return contentBuilder.toString();
+    }
+
+    private void writeFile(String fileName, String content) throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+        }
+    }
+}
